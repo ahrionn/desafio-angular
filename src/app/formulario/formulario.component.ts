43e01bcc0990915a9220c0cd71d9bedbd546dbc4
Luam Ahrion <luam_azf@hotmail.com>
2023-06-20 14:39:46 -0300

    Versão - Formulário

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -3,8 +3,37 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-formulario',
   templateUrl: './formulario.component.html',
-  styleUrls: ['./formulario.component.scss']
+  styleUrls: ['./formulario.component.css']
 })
-export class FormularioComponent {
 
+export class FormularioComponent {
+  nome: string = '';
+  sobrenome: string = '';
+  idade: number | null = null;
+  sexo: string = 'N';
+  dataNascimento: string = '';
+
+  constructor() {}
+
+  salvarDados() {
+    const dados = {
+      nome: this.nome,
+      sobrenome: this.sobrenome,
+      idade: this.idade,
+      sexo: this.sexo,
+      dataNascimento: this.dataNascimento
+    };
+
+    console.log('Dados preenchidos:', dados);
+
+    this.limparCampos();
+  }
+
+  limparCampos() {
+    this.nome = '';
+    this.sobrenome = '';
+    this.idade = null;
+    this.sexo = 'N';
+    this.dataNascimento = '';
+  }
 }
